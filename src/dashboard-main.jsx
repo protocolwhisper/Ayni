767234a46104ea05fd929ec2b8af33735afc5d96
ProtocolWhisper <chrismatai@hotmail.com>
2026-04-13 20:16:48 +0200

    enable dashboard

--- src/dashboard-main.jsx
+++ src/dashboard-main.jsx
@@ -1,1 +1,10 @@
-window.location.replace('/')
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import DashboardPage from './DashboardPage.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <DashboardPage />
+  </StrictMode>,
+)
